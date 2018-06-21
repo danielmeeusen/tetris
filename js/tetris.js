f296ff8397f0c00da07fe259610d476e4b4f8161
Daniel Meeusen <danmeeusen@gmail.com>
2018-06-21 15:59:09 -0700

    rotate

--- js/tetris.js
+++ js/tetris.js
@@ -9,10 +9,31 @@ const matrix = [
   [0, 1, 0]
 ];
 
+function collide(arena, player){
+  const [m, o] = [player.matrix, player.pos];
+  for(y=0; y<m.length; y++){
+    for(x=0; x < m[y].length; x++){
+      if(m[y][x] !== 0 && ( arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0){
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function createMatrix(w, h){
+  const matrix = [];
+  while(h--){
+    matrix.push(new Array(w).fill(0));
+  }
+  return matrix;
+}
+
 function draw(){
   context.fillStyle = '#000';
   context.fillRect(0, 0, canvas.width, canvas.height);
 
+  drawMatrix(arena, {x: 0, y: 0});
   drawMatrix(player.matrix, player.pos);
 }
 
@@ -27,9 +48,60 @@ function drawMatrix(matrix, offset){
   });
 }
 
+function merge(arena, player){
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if(value !== 0){
+        arena[y + player.pos.y][x + player.pos.x] = value;
+      }
+    });
+  });
+}
+
 function playerDrop(){
   player.pos.y++;
+  if(collide(arena, player)){
+    player.pos.y--;
+    merge(arena, player);
+    player.pos.y = 0;
+  }
   dropCounter = 0;
+}
+
+function playerMove(dir){
+  player.pos.x += dir;
+  if(collide(arena, player)){
+    player.pos.x -= dir;
+  }
+}
+
+function playerRotate(dir){
+  const pos = player.pos.x
+  let offset = 1;
+  rotate(player.matrix, dir);
+
+  while(collide(arena, player)){
+    player.pos.x += offset;
+    offset = -(offset + (offset > 0 ? 1 : -1));
+    if(offset > player.matrix[0].length) {
+      rotate(player.matrix, -dir);
+      player.pos.x = pos;
+      return;
+    }
+  }
+}
+
+function rotate(matrix, dir){
+  for(y=0; y < matrix.length; y++){
+    for(x=0; x < y; x++){
+      [ matrix[x][y], matrix[y][x], ] = [ matrix[y][x], matrix[x][y], ];
+    }
+  }
+  if(dir > 0){
+    matrix.forEach(row => row.reverse());
+  } else {
+    matrix.reverse();
+  }
 }
 
 let dropCounter = 0;
@@ -40,28 +112,32 @@ let lastTime = 0;
 function update(time = 0){
   const deltaTime = time - lastTime;
   lastTime = time;
-
   dropCounter += deltaTime;
   if(dropCounter > dropInterval) {
     playerDrop();
   }
-
   draw();
   requestAnimationFrame(update);
 }
 
+const arena = createMatrix(12, 20);
+
 const player = {
-  pos: {x: 5, y: 5},
-  matrix: matrix
+  pos: {x: 0, y: 0},
+  matrix: matrix,
 }
 
-document.addEventListener('keydown', event => {
-  if(event.keyCode === 37){
-    player.pos.x--;
-  }else if(event.keyCode === 39){
-    player.pos.x++;
-  } else if(event.keyCode === 40){
+document.addEventListener('keydown', e => {
+  if(e.keyCode === 37){
+    playerMove(-1)
+  }else if(e.keyCode === 39){
+    playerMove(1);
+  } else if(e.keyCode === 40){
     playerDrop();
+  } else if(e.keyCode === 81){
+    playerRotate(-1);
+  } else if(e.keyCode === 87 || e.keyCode === 38){
+    playerRotate(1);
   }
 });
 
