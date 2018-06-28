017ef4fba5fc69307b73add71fc9c5f44777e3f3
Daniel Meeusen <danmeeusen@gmail.com>
2018-06-27 19:14:52 -0700

    up too modal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+// $(document).ready(function(){
+//   $('#modal').modal();
+//   $('#modal').modal('open'); 
+// });
+
 const canvas = document.querySelector('.tetris');
 const context = canvas.getContext('2d');
 
